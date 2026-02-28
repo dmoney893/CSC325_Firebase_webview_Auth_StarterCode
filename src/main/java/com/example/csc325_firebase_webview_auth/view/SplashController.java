90665871e621b76e13533d08eb3db302c17d1b63
Daniel <gdaniel998@gmail.com>
2026-02-27 21:21:46 -0500

    Created the SplashController class

---
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/SplashController.java
@@ -0,0 +1,21 @@
+package com.example.csc325_firebase_webview_auth.view;
+
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXML;
+import javafx.util.Duration;
+
+public class SplashController {
+
+    @FXML
+    public void initialize() {
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        pause.setOnFinished(e -> {
+            try {
+                App.setRoot("/files/AccessFBView.fxml");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        pause.play();
+    }
+}
